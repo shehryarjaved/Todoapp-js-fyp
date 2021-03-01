3b9db3a2f4207b84ab92a55ddfa7f65047ade3f0
shehryarjaved <shehryarjaved007@gmail.com>
2021-03-01 23:49:13 +0500

    todoapp done

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+
+
+var list = document.getElementById("list");
+
+firebase.database().ref('todos').on('child_added',function(data){
+ // create li tag with text node
+ var li = document.createElement('li')
+ var liText = document.createTextNode(data.val().value)
+ li.appendChild(liText)
+ 
+
+ // create delete button
+ var delBtn = document.createElement("button")
+ var delText = document.createTextNode("DELETE")
+ delBtn.setAttribute("class", "btn btn-outline-primary demo")
+ delBtn.setAttribute("id",data.val().key)
+ delBtn.setAttribute("onclick", "deleteItem(this)")
+ delBtn.appendChild(delText)
+
+ // create edit button
+ var editBtn = document.createElement("button");
+ var editText = document.createTextNode("EDIT")
+ editBtn.setAttribute("class", "btn btn-outline-primary demo")
+ editBtn.setAttribute("id",data.val().key)
+ editBtn.appendChild(editText)
+ editBtn.setAttribute("onclick", "editItem(this)")
+
+
+ li.appendChild(delBtn)
+ li.appendChild(editBtn)
+
+ list.appendChild(li)
+    
+
+})
+
+function deleteItem(e) {
+    firebase.database().ref('todos').child(e.id).remove();
+    e.parentNode.remove()
+}
+
+function editItem(e) {
+  var val = prompt("Enter updated value",e.parentNode.firstChild.nodeValue)
+  var editodo ={
+
+    value : val,
+    key: e.id
+  }
+  firebase.database().ref('todos').child(e.id).set(editodo)
+
+  e.parentNode.firstChild.nodeValue = val;
+}
+
+function deleteAll() {
+    const db = firebase.database().ref('todos');
+    db.remove();
+    list.innerHTML = ""
+}
+
+// console.log(firebase)
+
+function addTodo() {
+    var todo_item = document.getElementById("todo-item");
+
+    var database =firebase.database().ref('todos')
+    var key=database.push().key;
+    var todo = {
+
+        value:todo_item.value,
+        key: key
+        
+
+
+    }
+    database.child(key).set(todo)
+    todo_item.value = ""
+}
+
+
